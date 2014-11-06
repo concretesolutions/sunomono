afae952079fe4fa5df6cc810419962413f2cada5
CSOscarTanner <oscar.tanner@concretesolutions.com.br>
2014-11-06 20:09:03 -0200

    Android: Added folder with the name of the day to keep reports history. Fixed problem with relative path of the report pictures.

--- lib/skeleton/config/scripts/android/run_tests_all_devices.sh
+++ lib/skeleton/config/scripts/android/run_tests_all_devices.sh
@@ -7,18 +7,29 @@
 
 
 ## CODE BEGIN  #############################################################
-echo Start: $(date)
+[ $# -ne 1 ] && echo "Wrong number of parameters." && exit 1
+
+echo Inicio da execução: $(date)
+
+reports_path="/var/www/html/reports/$(date +"%Y%d%m")"
+
 for device in $(adb devices | grep "device$" | cut -f 1)
 do
-  rm -r $WORKSPACE/reports/$device &> /dev/null
-  mkdir -p $WORKSPACE/reports/$device &> /dev/null
+  rm -r "$reports_path"/"$device" &> /dev/null
+  mkdir -p "$reports_path"/"$device" &> /dev/null
   echo $device | grep -q emulator
   # SET THE IGNORE TAGS TRUE IF THE TEST ARE RUNNING IN A DEVICE
   [ $? -ne 0 ] && ignore='--tags ~@ignore_if_test_in_device'
   cd $WORKSPACE
-  ADB_DEVICE_ARG=$device SCREENSHOT_PATH=$WORKSPACE/reports/$device/ calabash-android run $1 -p android --format 'Calabash::Formatters::Html' --out $WORKSPACE/reports/$device/reports.html $ignore &
+  ADB_DEVICE_ARG=$device SCREENSHOT_PATH="$reports_path"/"$device"/ calabash-android run $1 -p android --format 'Calabash::Formatters::Html' --out "$reports_path"/"$device"/reports.html $ignore &
 done
 wait
-echo End: $(date)
-echo 'Bye!'
+
+# Calabash has a problem with images relative path, the command above will replace all the images path on the
+# html report file to be a relative path
+for device in $(adb devices | grep "device$" | cut -f 1)
+do
+  sed -i 's|'"$reports_path"/"$device"/'||g' "$reports_path"/"$device"/reports.html
+done
+echo Fim da execução: $(date)
 ## CODE END  #############################################################
