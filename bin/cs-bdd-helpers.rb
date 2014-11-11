b75226cec44cffc6dfc2efa6d051cb61f37d0868
CSOscarTanner <oscar.tanner@concretesolutions.com.br>
2014-11-11 17:19:50 -0200

    Gem refactored to use Thor.

--- bin/cs-bdd-helpers.rb
+++ bin/cs-bdd-helpers.rb
@@ -1,75 +1,48 @@
-def print_usage
-  puts <<EOF
-  Usage: cs-bdd <command-name> [parameters]
-  <command-name> can be one of
-    new or n <project_name>
-      generate a project folder structure
-      \033[0;32mEx.: cs-bdd n project_name\033[0m
-    generate or g <type> <name>
-      generate a Feature or Step Definition or Screen file
-      <type> can be one of
-        feature -> Generate all files that composes an OS independenty feature
-        aFeature -> Generate all files that composes an Android feature
-        iFeature -> Generate all files that composes an iOS feature
-        step -> Generate an OS independenty step
-        aStep -> Generate a step that will be loaded only in Android
-        iStep -> Generate a step that will be loaded only in iOS
-        aScreen -> Generate an Android screen
-        iScreen -> Generate an iOS screen
-        \033[0;32mEx.: cs-bdd g aFeature Feature_name\033[0m
-    version or v
-      prints the gem version
-      \033[0;32mEx.: cs-bdd v\033[0m
-EOF
-end
-
 def create_feature_file name, platform = nil
-  # Getting the contents of the example.feature file
-  content = File.read( File.join( @examples_dir, "example.feature" ) )
-  
-  # Updating the contents with the name passed as an option
-  content = content.gsub( "|Name|", name.capitalize )
+
+  # options used to generate the file in the template function
+  opts = {:name => camelize(name)}
 
   # If platform is not nil than the feature is OS dependent
-  file = nil
+  file_path = ""
   if platform.nil?
-    content = content.gsub( "|Platform|", "") # Don't show any indication of platform inside the feature file
-    file = File.join( @features_dir, "#{name.downcase}.feature" )
+    file_path = File.join( FileUtils.pwd, "features", "#{name.downcase}.feature" )
+    opts[:platform] = ""
   else
-    content = content.gsub( "|Platform|", "(#{platform} Only)") # Indicates the platform in the title of the feature
-    file = File.join( @features_dir, platform.downcase, "features", "#{name.downcase}.feature" )
+    file_path = File.join( FileUtils.pwd, "features", platform.downcase, "features", "#{name.downcase}.feature" )
+    opts[:platform] = platform
   end
 
-  # Creating the feature file with the updated content
-  File.open( file, 'w' ) { |file| file.puts content }
+  # Thor creates a file based on the templates/feature.tt template
+  template( "feature", file_path, opts )
 end
 
 def create_steps_file name, platform = nil
-  # Getting the contents of the example_steps.rb file
-  content = File.read( File.join( @examples_dir, "example_steps.rb" ) )
-  
-  # If platform is not nil than the feature is OS dependent
-  file = nil
+
+  # options used to generate the file in the template function
+  opts = {:name => camelize(name)}  
+
+  # If platform is not nil than the step is OS dependent
+  file_path = nil
   if platform.nil?
-    file = File.join( @steps_dir, "#{name.downcase}_steps.rb" )
+    file_path = File.join( FileUtils.pwd, "features", "step_definitions", "#{name.downcase}_steps.rb" )
+    opts[:platform] = ""
   else
-    file = File.join( @features_dir, platform.downcase, "steps_definition", "#{name.downcase}_steps.rb" )
+    file_path = File.join( FileUtils.pwd, "features", platform.downcase, "step_definitions", "#{name.downcase}_steps.rb" )
+    opts[:platform] = platform
   end
 
-  # Creating the steps file with the default content
-  File.open( file, 'w' ) { |file| file.puts content }
+  # Thor creates a file based on the templates/steps.tt template
+  template( "steps", file_path, opts )
 end
 
 def create_screen_file name, platform
-  # Getting the contents of the example_screen.rb file
-  content = File.read( File.join( @examples_dir, "example_screen.rb" ) )
-  
-  # Updating the contents with the name passed as an option
-  content = content.gsub( "|Name|", name.capitalize )
-  content = content.gsub( "|Platform|", platform )
-  
-  # Creating the feature file with the updated content
-  File.open( File.join( @features_dir, platform.downcase, "screens", "#{name.downcase}_screen.rb" ), 'w' ) { |file| file.puts content }
+
+  # options used to generate the file in the template function
+  opts = {:name => camelize(name), :platform => platform}
+
+  # Thor creates a file based on the templates/screen.tt template
+  template( "screen", File.join( FileUtils.pwd, "features", platform.downcase, "screens", "#{name.downcase}_screen.rb"), opts )
 end
 
 def camelize string
