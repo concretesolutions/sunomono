a7565fadeb3f32d2fd93258324b9d7dcbadc8954
Oscar Tanner <oscar.tanner@concretesolutions.com.br>
2016-01-29 09:31:20 -0200

    Updated ios build app script
    Updated the script to support the version of the iOS SDK 9.2.
    This new SDK version needs the parameter destination to build the app

--- lib/skeleton/config/scripts/ios/build_app.rb
+++ lib/skeleton/config/scripts/ios/build_app.rb
@@ -3,13 +3,13 @@
 # ----------------------------------------------------------------------------
 #
 # $1 -> configuration environment (dev or jenkins)
-# $2 -> device type. Builds for 'simulator' or 'device'
 #
 #
 # REMEMBER to fill the configuration file build_app.yml
 
 require 'fileutils'
 require 'yaml'
+require 'pathname'
 
 # When running on CI
 # It is a good pratice to run pod install when executing this script
@@ -19,11 +19,10 @@ require 'yaml'
 # Parsing the yaml configuration file
 config = YAML.load_file(File.join(File.dirname(__FILE__), 'build_app.yml'))
 
-if ARGV.length < 2
+if ARGV.length != 1
   puts 'Error: Wrong number of arguments!'
-  puts 'Usage: build_app.rb environment device_type'
+  puts 'Usage: build_app.rb environment'
   puts "Available Environments: #{config.keys.join(', ')}"
-  puts "Device type: 'simulator' or 'device'"
   exit 1
 end
 
@@ -37,29 +36,36 @@ else
   config = config[ARGV[0]]
 end
 
-export_path = File.join(config['export_path'], ARGV[1])
+# Creating a folder name from the destination configuration parameter
+folder_name = config['destination'].gsub('platform=', '').gsub('name=', '')
+              .tr(' ', '_').tr(',', '_')
+export_path = File.join(config['export_path'], folder_name)
+
+# Removing the folder where the .app will be stored if it already exists
+FileUtils.rm_r export_path if Dir.exist?(export_path)
 
 # Creating the folder where the .app will be stored
 FileUtils.mkdir_p export_path
-
-# Choosing the SDK for device or simulator
-sdk = ''
-if ARGV[1] == 'device'
-  sdk = 'iphoneos'
-else
-  sdk = 'iphonesimulator'
-end
 
 puts 'Building project'
 
 system <<eos
   xcodebuild -workspace "#{config['xcworkspace']}" \
-  -scheme "#{config['scheme']}" -sdk "#{sdk}" \
+  -scheme "#{config['scheme']}" -destination "#{config['destination']}" \
   -configuration "#{config['configuration']}" clean build \
   CONFIGURATION_BUILD_DIR="#{export_path}"
 eos
 
-puts "APP_BUNDLE_PATH=#{File.join(export_path, config['scheme'])}.app"
+# Getting the app folder that was created
+# Listing all folders on the export path folder
+folders = Pathname.new(export_path).children.select { |c| c.directory? }
+# Getting the folder which ends with .app
+app_pathname = folders.select { |f| f.to_s.match('.app$') }
+# Getting the app folder path
+app_path = app_pathname.first.to_s
+
+# Printing the APP_BUNDLE_PATH in the terminal
+puts "APP_BUNDLE_PATH=#{app_path}"
 
 puts "End: #{Time.now.strftime('%H:%M:%S')}"
 puts 'Bye!'
