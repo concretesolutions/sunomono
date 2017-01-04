1a86b13994d20e5f6863a3bd0f5e72c14dbcb890
Wellington <wellington.santos@concrete.com.br>
2017-01-04 21:25:18 -0200

    New improvements to sunomono tests
    - Adding improviment to dont print output in terminal
    - Getting gem version from output terminal
    - New organize
    - Change the names of contexts to make sense

--- spec/sunomono_test.rb
+++ spec/sunomono_test.rb
@@ -1,4 +1,5 @@
 require 'sunomono'
+require 'open3'
 
 describe Sunomono do
 
@@ -45,40 +46,6 @@ describe Sunomono do
         expect(Dir.entries("#{@project_name}/config/scripts/ios")).
             to include('.', '..', 'build_app.rb', 'build_app.yml', 'devices.txt', 'run_tests_all_devices.sh')
       end
-
-      it 'Create new project using alias command' do
-        system "suno new '#{@project_name}'", :out => File::NULL
-
-        expect(Dir.entries(@project_name)).
-            to include('screenshots', 'features', 'config', 'README.md', 'Gemfile', '.gitignore')
-        # Features files
-        expect(Dir.entries("#{@project_name}/features")).
-            to include('android', 'ios', 'step_definitions', 'support')
-        expect(Dir.entries("#{@project_name}/features/android")).
-            to include('android_screen_base.rb', 'features', 'screens', 'step_definitions', 'support')
-        expect(Dir.entries("#{@project_name}/features/ios")).
-            to include('ios_screen_base.rb', 'features', 'screens', 'step_definitions', 'support')
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            to include('base_steps.rb')
-        expect(Dir.entries("#{@project_name}/features/support")).
-            to include('env.rb', 'exceptions.rb')
-
-        # Config files
-        expect(Dir.entries("#{@project_name}/config")).
-            to include('cucumber.yml', 'email', 'load_classes.rb', 'scripts')
-
-        # Email files
-        expect(Dir.entries("#{@project_name}/config/email")).
-            to include('template.html')
-
-        # Scripts files
-        expect(Dir.entries("#{@project_name}/config/scripts")).
-            to include('android', 'break_build_if_failed.sh', 'check_if_tests_failed.sh', 'ios')
-        expect(Dir.entries("#{@project_name}/config/scripts/android")).
-            to include('run_tests_all_devices.sh', 'start_emulators.sh', 'stop_emulators.sh')
-        expect(Dir.entries("#{@project_name}/config/scripts/ios")).
-            to include('.', '..', 'build_app.rb', 'build_app.yml', 'devices.txt', 'run_tests_all_devices.sh')
-      end
     end
 
     context 'Try creates new project with a invalid argument' do
@@ -92,25 +59,37 @@ describe Sunomono do
 
     context 'Should return the latest version of sunonomo' do
       it 'Returns the latest version' do
-        system "sunomono version", :out => File::NULL
-        version = "#{Sunomono::VERSION}"
+        stdout = Open3.capture3("sunomono version")
+        version = "Sunomono Version " + "#{Sunomono::VERSION}"
 
         expect(version).
-            to end_with('0.2.0.pre')
-      end
-
-
-      it 'Should return the latest version of sunonomo using alias command' do
-        system "suno version", :out => File::NULL
-        version = "#{Sunomono::VERSION}"
-
-        expect(version).
-            to end_with('0.2.0.pre')
+            to include(stdout.first.gsub(/\n/, ""))
       end
     end
 
-    context 'Using I18n to translate files' do
-      it 'Generate an OS independent feature using I18n to portuguese language' do
+    context 'Generate Feature' do
+      it 'Generates all files' do
+        system "sunomono new '#{@project_name}'", :out => File::NULL
+
+        Dir.chdir(@project_name)
+
+        feature_name = 'rspec_sunomono'
+
+        system "sunomono generate feature '#{feature_name}'", :out => File::NULL
+
+        Dir.chdir('../')
+
+        expect(Dir.entries("#{@project_name}/features")).
+            to include("#{feature_name}.feature")
+        expect(Dir.entries("#{@project_name}/features/step_definitions")).
+            to include("#{feature_name}_steps.rb")
+        expect(Dir.entries("#{@project_name}/features/android/screens")).
+            to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("#{@project_name}/features/ios/screens")).
+            to include("#{feature_name}_screen.rb")
+      end
+
+      it 'Generate feature in pt' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
         feature_name = 'sunomono'
@@ -127,7 +106,7 @@ describe Sunomono do
             to include("######### DADO #########\n", "\n", "######### QUANDO #########\n", "\n", "######### ENTãO #########")
       end
 
-      it 'Generate an OS independent feature using english language' do
+      it 'Generate feature in en' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
         feature_name = 'sunomono'
@@ -143,52 +122,8 @@ describe Sunomono do
         expect(File.readlines("#{@project_name}/features/step_definitions/#{feature_name}_steps.rb")).
             to include("######### GIVEN #########\n", "\n", "######### WHEN #########\n", "\n", "######### THEN #########")
       end
-    end
 
-    context 'This command should generates an OS independent feature(all files)' do
-      it 'Generates an OS independent feature' do
-        system "sunomono new '#{@project_name}'", :out => File::NULL
-
-        Dir.chdir(@project_name)
-
-        feature_name = 'rspec_sunomono'
-
-        system "sunomono generate feature '#{feature_name}'", :out => File::NULL
-
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            to include('.', '..', 'android', 'ios', 'step_definitions', 'support', "#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            to include("#{feature_name}_screen.rb")
-      end
-
-      it 'Generates an OS independent feature with alias command suno' do
-        system "suno new '#{@project_name}'", :out => File::NULL
-
-        Dir.chdir(@project_name)
-
-        feature_name = 'rspec_sunomono'
-
-        system "suno generate feature '#{feature_name}'", :out => File::NULL
-
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            to include('.', '..', 'android', 'ios', 'step_definitions', 'support', "#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            to include("#{feature_name}_screen.rb")
-      end
-
-      it 'Generates an OS independent feature with alias command g ' do
+      it 'Generates with alias command g' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
         Dir.chdir(@project_name)
@@ -200,7 +135,7 @@ describe Sunomono do
         Dir.chdir('../')
 
         expect(Dir.entries("#{@project_name}/features")).
-            to include('.', '..', 'android', 'ios', 'step_definitions', 'support', "#{feature_name}.feature")
+            to include("#{feature_name}.feature")
         expect(Dir.entries("#{@project_name}/features/step_definitions")).
             to include("#{feature_name}_steps.rb")
         expect(Dir.entries("#{@project_name}/features/android/screens")).
@@ -210,8 +145,8 @@ describe Sunomono do
       end
     end
 
-    context 'Try Generates an OS independent feature with a invalid argument' do
-      it 'Independent feature will be not created' do
+    context 'Try Generates a feature with a invalid argument' do
+      it 'Feature will be not created' do
         system "sunomono new '#{@project_name}'", :out => File::NULL
 
         feature_name = 'rspec_sunomono'
@@ -232,7 +167,7 @@ describe Sunomono do
             not_to include("#{feature_name}_screen.rb")
       end
 
-      it 'Independent feature will be not created using alias command suno with a invalid argument' do
+      it 'Feature will be not created using alias command suno with a invalid argument' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
         feature_name = 'rspec_sunomono'
@@ -254,7 +189,7 @@ describe Sunomono do
       end
 
 
-      it 'Independent feature will be not created using alias command  g with a invalid argument ' do
+      it 'Feature will be not created using alias command with a invalid argument' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
         feature_name = 'rspec_sunomono'
@@ -277,7 +212,7 @@ describe Sunomono do
     end
 
     context 'Generates an OS independent step' do
-      it 'Command cant generates .feature and screens files' do
+      it 'Cant generates .feature and screens files' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
@@ -297,8 +232,8 @@ describe Sunomono do
       end
     end
 
-    context 'Generates an OS indenpendat screen' do
-      it 'Comamand cant generates .feature and step_definition files ' do
+    context 'Generates an OS indenpendent screen' do
+      it 'Cant generates .feature and step_definition files' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
@@ -317,14 +252,14 @@ describe Sunomono do
     end
 
     context 'commands to generates an Android dependent files' do
-      it 'return created folders to android plataform' do
+      it 'Create folders to android plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
 
         Dir.chdir(@project_name)
 
-        system "suno g android-feature #{feature_name}", :out => File::NULL
+        system "suno generate android-feature #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
@@ -336,14 +271,14 @@ describe Sunomono do
             to include("#{feature_name}_screen.rb")
       end
 
-      it 'return created screen to android plataform' do
+      it 'Create screen to android plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunonomo'
 
         Dir.chdir(@project_name)
 
-        system "suno g android-screen #{feature_name}", :out => File::NULL
+        system "suno generate android-screen #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
@@ -353,14 +288,14 @@ describe Sunomono do
             not_to include("#{feature_name}_steps.rb")
       end
 
-      it 'retrun created step to android plataform' do
+      it 'Create step to android plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
 
         Dir.chdir(@project_name)
 
-        system "suno g android-step #{feature_name}", :out => File::NULL
+        system "suno generate android-step #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
@@ -372,14 +307,14 @@ describe Sunomono do
     end
 
     context 'commands to generates an IOS dependent files' do
-      it 'return created folders to IOS plataform' do
+      it 'Create folders to IOS plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
 
         Dir.chdir(@project_name)
 
-        system "suno g ios-feature #{feature_name}", :out => File::NULL
+        system "suno generate ios-feature #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
@@ -391,14 +326,14 @@ describe Sunomono do
             to include("#{feature_name}_screen.rb")
       end
 
-      it 'return created screen to IOS plataform' do
+      it 'Create screen to IOS plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
 
         Dir.chdir(@project_name)
 
-        system "suno g ios-screen #{feature_name}", :out => File::NULL
+        system "suno generate ios-screen #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
@@ -408,14 +343,14 @@ describe Sunomono do
             not_to include("#{feature_name}_steps.rb")
       end
 
-      it 'return created step to IOS plataform' do
+      it 'Create step to IOS plataform' do
         system "suno new #{@project_name}", :out => File::NULL
 
         feature_name = 'sunomono'
 
         Dir.chdir(@project_name)
 
-        system "suno g ios-step #{feature_name}", :out => File::NULL
+        system "suno generate ios-step #{feature_name}", :out => File::NULL
 
         Dir.chdir('../')
 
