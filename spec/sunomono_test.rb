d5ae3f5d933321b7b961e348f77b043c4cb1e2eb
Wellington <wellington.santos@concrete.com.br>
2017-01-06 16:31:44 -0200

    Refactor tests cases
    - Remove several unecessary code
    - New improviment to get gem version

--- spec/sunomono_test.rb
+++ spec/sunomono_test.rb
@@ -1,20 +1,21 @@
 require 'sunomono'
-require 'open3'
 
 describe Sunomono do
 
   before(:each) do
     @project_name = 'Sunomono_test'
+    @feature_name = 'sunomono'
   end
 
   after(:each) do
-    FileUtils.rm_rf(@project_name)
+    Dir.chdir('..') unless Dir.exist?(@project_name)
+       FileUtils.rm_rf(@project_name)
   end
 
   describe 'Sunomono gem commands' do
     context 'Returns project created with all files' do
       it 'Create new project using default command' do
-        system "sunomono new '#{@project_name}'", :out => File::NULL
+        system "sunomono new '#{@project_name}' > /dev/null"
 
         expect(Dir.entries(@project_name)).
             to include('screenshots', 'features', 'config', 'README.md', 'Gemfile', '.gitignore')
@@ -50,7 +51,7 @@ describe Sunomono do
 
     context 'Try creates new project with a invalid argument' do
       it 'Project will not be generated' do
-        system "suno new '#{@project_name}' invalid argument", :out => File::NULL
+        system "suno new '#{@project_name}' invalid argument  > /dev/null"
 
         expect(Dir.entries(".")).
             not_to include("#{@project_name}")
@@ -59,308 +60,247 @@ describe Sunomono do
 
     context 'Should return the latest version of sunonomo' do
       it 'Returns the latest version' do
-        stdout = Open3.capture3("sunomono version")
+        stdout = `sunomono version`
         version = "Sunomono Version " + "#{Sunomono::VERSION}"
 
         expect(version).
-            to include(stdout.first.gsub(/\n/, ""))
+            to include(stdout.gsub(/\n/, ""))
       end
     end
 
     context 'Generate Feature' do
       it 'Generates all files' do
-        system "sunomono new '#{@project_name}'", :out => File::NULL
+        system "sunomono new '#{@project_name}'  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        feature_name = 'rspec_sunomono'
+        system "sunomono generate feature '#{@feature_name}'  > /dev/null"
 
-        system "sunomono generate feature '#{feature_name}'", :out => File::NULL
-
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/screens")).
+            to include("#{@feature_name}_screen.rb")
       end
 
       it 'Generate feature in pt' do
-        system "suno new '#{@project_name}'", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new '#{@project_name}' > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate feature '#{feature_name}' --lang=pt", :out => File::NULL
+        system "suno generate feature '#{@feature_name}' --lang=pt > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(File.readlines("#{@project_name}/features/#{feature_name}.feature")).
-            to include("# language: pt\n", "Funcionalidade: #{feature_name.capitalize} \n", "\n", "  Contexto:\n", "    # Insira os passos\n", "    \n", "  Cenário: Primeiro Cenario\n", "    # Insira os passos\n")
-        expect(File.readlines("#{@project_name}/features/step_definitions/#{feature_name}_steps.rb")).
+        expect(File.readlines("features/#{@feature_name}.feature")).
+            to include("# language: pt\n", "Funcionalidade: #{@feature_name.capitalize} \n", "\n", "  Contexto:\n", "    # Insira os passos\n", "    \n", "  Cenário: Primeiro Cenario\n", "    # Insira os passos\n")
+        expect(File.readlines("features/step_definitions/#{@feature_name}_steps.rb")).
             to include("######### DADO #########\n", "\n", "######### QUANDO #########\n", "\n", "######### ENTãO #########")
       end
 
       it 'Generate feature in en' do
-        system "suno new '#{@project_name}'", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new '#{@project_name}' > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate feature '#{feature_name}'", :out => File::NULL
+        system "suno generate feature '#{@feature_name}' > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(File.readlines("#{@project_name}/features/#{feature_name}.feature")).
-            to include("# language: en\n", "Feature: #{feature_name.capitalize} \n", "\n", "  Background:\n", "    # Insert steps\n", "    \n", "  Scenario: First Scenario\n", "    # Insert steps\n")
-        expect(File.readlines("#{@project_name}/features/step_definitions/#{feature_name}_steps.rb")).
+        expect(File.readlines("features/#{@feature_name}.feature")).
+            to include("# language: en\n", "Feature: #{@feature_name.capitalize} \n", "\n", "  Background:\n", "    # Insert steps\n", "    \n", "  Scenario: First Scenario\n", "    # Insert steps\n")
+        expect(File.readlines("features/step_definitions/#{@feature_name}_steps.rb")).
             to include("######### GIVEN #########\n", "\n", "######### WHEN #########\n", "\n", "######### THEN #########")
       end
 
       it 'Generates with alias command g' do
-        system "suno new '#{@project_name}'", :out => File::NULL
+        system "suno new '#{@project_name}'  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        feature_name = 'rspec_sunomono'
+        system "suno g feature '#{@feature_name}'  > /dev/null"
 
-        system "suno g feature '#{feature_name}'", :out => File::NULL
-
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/screens")).
+            to include("#{@feature_name}_screen.rb")
       end
     end
 
     context 'Try Generates a feature with a invalid argument' do
       it 'Feature will be not created' do
-        system "sunomono new '#{@project_name}'", :out => File::NULL
-
-        feature_name = 'rspec_sunomono'
+        system "sunomono new '#{@project_name}'  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "sunomono generate feature '#{feature_name}' invalid_argument", :out => File::NULL
+        system "sunomono generate feature '#{@feature_name}' invalid_argument  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            not_to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            not_to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/screens")).
+            not_to include("#{@feature_name}_screen.rb")
       end
 
       it 'Feature will be not created using alias command suno with a invalid argument' do
         system "suno new '#{@project_name}'", :out => File::NULL
 
-        feature_name = 'rspec_sunomono'
-
         Dir.chdir(@project_name)
 
-        system "suno generate feature '#{feature_name}' invalid_argument", :out => File::NULL
+        system "suno generate feature '#{@feature_name}' invalid_argument  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            not_to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            not_to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/screens")).
+            not_to include("#{@feature_name}_screen.rb")
       end
 
 
       it 'Feature will be not created using alias command with a invalid argument' do
-        system "suno new '#{@project_name}'", :out => File::NULL
-
-        feature_name = 'rspec_sunomono'
+        system "suno new '#{@project_name}'  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno g feature '#{feature_name}' invalid_argument", :out => File::NULL
+        system "suno g feature '#{@feature_name}' invalid_argument  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            not_to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            not_to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/screens")).
+            not_to include("#{@feature_name}_screen.rb")
       end
     end
 
     context 'Generates an OS independent step' do
       it 'Cant generates .feature and screens files' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "sunomono generate step #{feature_name}", :out => File::NULL
+        system "sunomono generate step #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/android")).
-            not_to include("#{feature_name}_screen.rb")
-        expect(Dir.entries("#{@project_name}/features/ios")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/android")).
+            not_to include("#{@feature_name}_screen.rb")
+        expect(Dir.entries("features/ios")).
+            not_to include("#{@feature_name}_screen.rb")
       end
     end
 
     context 'Generates an OS indenpendent screen' do
       it 'Cant generates .feature and step_definition files' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "sunomono generate screen #{feature_name}", :out => File::NULL
+        system "sunomono generate screen #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
+        expect(Dir.entries("features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
       end
     end
 
     context 'commands to generates an Android dependent files' do
       it 'Create folders to android plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate android-feature #{feature_name}", :out => File::NULL
+        system "suno generate android-feature #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/android/features")).
-            to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/android/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features/android/features")).
+            to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/android/step_definitions")).
+            to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/android/screens")).
+            to include("#{@feature_name}_screen.rb")
       end
 
       it 'Create screen to android plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunonomo'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate android-screen #{feature_name}", :out => File::NULL
+        system "suno generate android-screen #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/android/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/android/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
+        expect(Dir.entries("features/android/features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/android/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
       end
 
       it 'Create step to android plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate android-step #{feature_name}", :out => File::NULL
+        system "suno generate android-step #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/android/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/android/screens")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features/android/features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/android/screens")).
+            not_to include("#{@feature_name}_screen.rb")
       end
     end
 
     context 'commands to generates an IOS dependent files' do
       it 'Create folders to IOS plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate ios-feature #{feature_name}", :out => File::NULL
+        system "suno generate ios-feature #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/ios/features")).
-            to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/ios/step_definitions")).
-            to include("#{feature_name}_steps.rb")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/features")).
+            to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/ios/step_definitions")).
+            to include("#{@feature_name}_steps.rb")
+        expect(Dir.entries("features/ios/screens")).
+            to include("#{@feature_name}_screen.rb")
       end
 
       it 'Create screen to IOS plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate ios-screen #{feature_name}", :out => File::NULL
+        system "suno generate ios-screen #{@feature_name}  > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/ios/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/ios/step_definitions")).
-            not_to include("#{feature_name}_steps.rb")
+        expect(Dir.entries("features/ios/features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/ios/step_definitions")).
+            not_to include("#{@feature_name}_steps.rb")
       end
 
       it 'Create step to IOS plataform' do
-        system "suno new #{@project_name}", :out => File::NULL
-
-        feature_name = 'sunomono'
+        system "suno new #{@project_name}  > /dev/null"
 
         Dir.chdir(@project_name)
 
-        system "suno generate ios-step #{feature_name}", :out => File::NULL
+        system "suno generate ios-step #{@feature_name} > /dev/null"
 
-        Dir.chdir('../')
-
-        expect(Dir.entries("#{@project_name}/features/ios/features")).
-            not_to include("#{feature_name}.feature")
-        expect(Dir.entries("#{@project_name}/features/ios/screens")).
-            not_to include("#{feature_name}_screen.rb")
+        expect(Dir.entries("features/ios/features")).
+            not_to include("#{@feature_name}.feature")
+        expect(Dir.entries("features/ios/screens")).
+            not_to include("#{@feature_name}_screen.rb")
       end
     end
   end
 end
-
 
