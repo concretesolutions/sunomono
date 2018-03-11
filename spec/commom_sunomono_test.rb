6a50d10db801a3c198972f019586ffb91797c399
Wellington Avelino <wellington.santos@concrete.com.br>
2018-03-10 21:12:24 -0300

    Fix common test

--- spec/commom_sunomono_test.rb
+++ spec/commom_sunomono_test.rb
@@ -5,12 +5,7 @@ describe Sunomono do
   before(:each) do
     @directory     = Dir.pwd
   end
-
-  after(:each) do
-    Dir.chdir(@directory)
-    FileUtils.rm_rf(@project_name)
-  end
-
+  
   context 'Should return the latest version of sunonomo' do
     it 'Returns the latest version' do
       stdout  = `sunomono version`
